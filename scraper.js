361e392fcbaabebfdcf08f9c371be0bebde892a3
Knut Hühne <knut@k-nut.eu>
2017-07-24 17:31:49 +0200

    styling

--- scraper.js
+++ scraper.js
@@ -1,14 +1,14 @@
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-const fetch = require('isomorphic-fetch');
+const fetch = require("isomorphic-fetch");
 const _ = require("lodash");
-const fs = require('fs');
+const fs = require("fs");
 
-const getForOffset = (offset) => {
-    const url = `http://www.bundestag.de/ajax/filterlist/de/dokumente/protokolle/plenarprotokolle/-/442112/h_6810466be65964217012227c14bad20f?limit=10&noFilterSet=true&offset=${offset}`
+const getForOffset = offset => {
+    const url = `http://www.bundestag.de/ajax/filterlist/de/dokumente/protokolle/plenarprotokolle/-/442112/h_6810466be65964217012227c14bad20f?limit=10&noFilterSet=true&offset=${offset}`;
     return fetch(url)
         .then(response => {
-            console.log(`Finished downloading page with offset ${offset}`)
+            console.log(`Finished downloading page with offset ${offset}`);
             if (response.status >= 400) {
                 throw new Error("Bad response from server");
             }
@@ -18,28 +18,33 @@ const getForOffset = (offset) => {
             return new JSDOM(html);
         })
         .catch(console.error);
-}
+};
 
-const getTops = (dom) => {
+const getTops = dom => {
     const trs = dom.window.document.querySelectorAll(".bt-table-data tbody tr");
     return Array.from(trs).map(tr => {
         const lis = tr.querySelectorAll(".bt-top-liste > li");
         const session = tr.querySelector("td:nth-child(2)").textContent;
-        const sessionData =  Array.from(lis).map(li => {
+        const sessionData = Array.from(lis).map(li => {
             const agg = {};
-            agg['topic'] = li.querySelector("strong").textContent.trim();
-            agg['speakers'] = Array.from(li.querySelectorAll(".bt-redner-liste strong")).map(strong => strong.textContent);
-            return agg;})
-        return {'tops': sessionData, session}
-    })
-}
+            agg["topic"] = li.querySelector("strong").textContent.trim();
+            agg["speakers"] = Array.from(
+                li.querySelectorAll(".bt-redner-liste strong")
+            ).map(strong => strong.textContent);
+            return agg;
+        });
+        return { tops: sessionData, session };
+    });
+};
 
-const requests = []
-for (let offset=0; offset<250; offset += 10){
-    requests.push(getForOffset(offset))
+const requests = [];
+for (let offset = 0; offset < 250; offset += 10) {
+    requests.push(getForOffset(offset));
 }
 Promise.all(requests).then(promises => {
-    const res = _.flatMap(promises, data => getTops(data))
+    const res = _.flatMap(promises, data => getTops(data));
     const json = JSON.stringify(res, null, 2);
-    fs.writeFile('/Users/knut/Desktop/all_topics.json', json, 'utf8', () => console.log("Done Writing! ✍️"));
+    fs.writeFile("/Users/knut/Desktop/all_topics.json", json, "utf8", () =>
+        console.log("Done Writing! ✍️")
+    );
 });
